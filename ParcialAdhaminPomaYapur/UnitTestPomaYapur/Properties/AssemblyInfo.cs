ad77e7f65438d87d157ec54bc88d9b0f232fae1b
AdhaminRubenPomaYapur <a2019115301@estudiantes.upsa.edu.bo>
2020-09-19 12:43:07 -0400

    Terminado el unitest

---
+++ ParcialAdhaminPomaYapur/UnitTestPomaYapur/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPomaYapur")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestPomaYapur")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85485b68-a5fd-41fd-bc25-43efb18d230d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ParcialAdhaminPomaYapur/UnitTestPomaYapur/UnitTestPoma.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcialAdhaminPomaYapur.Controllers;
+using ParcialAdhaminPomaYapur.Models;
+
+namespace UnitTestPomaYapur
+{
+    [TestClass]
+    public class UnitTestPoma
+    {
+        [TestMethod]
+        public void TestMethodGet()
+        {
+            //Arange
+            CountriesController countriesController = new CountriesController();
+            Country country_expected = new Country()
+            {
+                alpha3Code = "Col",
+                region = "Americas",
+                name = "Colombia",
+                area = 1258639,
+                CallingCodes = 57,
+                languages = "Castellano",
+                flag = "https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Bolivia.svg"
+
+            };
+            //Act
+            var listado = countriesController.GetCountries();
+            var respuesta = countriesController.GetCountry(country_expected.alpha3Code);
+            var actual = respuesta as OkNegotiatedContentResult<Country>;
+            //Assert
+            Assert.IsNotNull(listado);
+            Assert.IsNull(actual);
+            
+        }
+    }
+}
